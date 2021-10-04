26b12db93a50f3c3b51c93eab8c1a47c7b7ee7be
jhoelssf <jhoelssflorez@gmail.com>
2021-10-03 19:59:01 -0500

    first commit

---
+++ useForm.tsx
@@ -0,0 +1,25 @@
+import {ChangeEvent, useState} from 'react'
+
+export const useForm =<T extends Object>(initState: T) => {
+
+    const [form, setForm] = useState(initState)
+
+    const reset = () => {
+        setForm(initState)
+    }
+
+    const handleChange = ({target}: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = target
+        setForm({
+            ...form,
+            [name]: value
+        })
+    }
+
+    return {
+        form,
+        handleChange,
+        reset,
+        ...form
+    }
+}
